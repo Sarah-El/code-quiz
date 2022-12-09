94137f2319e82d3f23765addae6207c0fcf01bab
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2022-12-09 19:12:53 +0000

    added query selector variables to logic js file

---
+++ assets/js/logic.js
@@ -0,0 +1,20 @@
+// Query selector variables from index HTML
+var startBtn = document.querySelector("#start");
+var submitBtn = document.querySelector("#submit");
+var timer = document.querySelector("#time");
+var choices = document.querySelector("#choices");
+var initials = document.querySelector("#initials");
+var finalscore = document.querySelector("#final-score");
+var initialsEl = document.querySelector("initials");
+
+
+
+// Import sound effects
+var sfxRight = new Audio("assets/sfx/correct.wav");
+var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+
+// play "wrong" sound effect
+sfxWrong.play();
+
+// play "right" sound effect
+sfxRight.play();
